f72c7eaa7398f3992eff09371b6a653b45f0eb9d
Fiona Wood <fwood@college.harvard.edu>
2013-11-26 15:50:47 -0500

    test php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+	$dbHost = "mysql.cs179.org";
+	$dbPort = 3306;
+	$dbUsername = "cs279_zabawa";
+	$dbPass = "1ntrinsic!";
+	$dbName = "db_zabawa";
+?>
---
+++ js/setup.js
@@ -0,0 +1,24 @@
+function shuffle(array) {
+    var counter = array.length, temp, index;
+
+    // While there are elements in the array
+    while (counter--) {
+        // Pick a random index
+        index = (Math.random() * (counter + 1)) | 0;
+
+        // And swap the last element with it
+        temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
+}
+
+function makeURL(page) {
+  //var x = "http://zabawa.cs179.org/";
+  var x = "file://localhost/Users/fionawwood/Documents/zabawa/";
+  x=x.concat(page);
+  x=x.concat(".html");
+  return x;
+}
