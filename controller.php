886113aae16ad59cfb8f9c87272077be1ff96d9f
Fiona Wood <fwood@college.harvard.edu>
2013-11-26 16:23:53 -0500

    array oops take 2

--- controller.php
+++ controller.php
@@ -12,7 +12,7 @@
 	echo $game_sequence;
 	$mysqli->query("INSERT INTO user (id, first_game, second_game, third_game) VALUES ('$SESSION_ID', '$game_sequence[0]', '$game_sequence[1]', '$game_sequence[2]')");
 
-	$incentive_sequence = ['badges','levels','leader'];
+	$incentive_sequence = array('badges','levels','leader');
 	shuffle($incentive_sequence);
 	$mysqli->query("INSERT INTO user (id, first_incentive, second_incentive, third_incentive) VALUES ('$SESSION_ID', '$incentive_sequence[0]', '$incentive_sequence[1]', '$incentive_sequence[2]')");
 
