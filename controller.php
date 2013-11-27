c8f78e4cb1b706dde9ca7ec7f97ec6df84d1c9d9
Fiona Wood <fwood@college.harvard.edu>
2013-11-27 12:05:50 -0500

    more debugging

--- controller.php
+++ controller.php
@@ -2,6 +2,7 @@
 	session_start();
 	include "config.php";
 	$SESSION_ID = $_SESSION['id'];
+	echo "hello";
 	echo $SESSION_ID;
 
 	$mysqli = new mysqli("$dbHost", "$dbUsername", "$dbPass", "$dbName");
@@ -9,9 +10,15 @@
 	$game_sequence = array('text-adventure','word-bounce','word-scramble');
 	shuffle($game_sequence);
 	echo $game_sequence[0];
+	echo $game_sequence[1];
+	echo $game_sequence[2];
+
 	$mysqli->query("INSERT INTO user (id, first_game, second_game, third_game) VALUES ('$SESSION_ID', '$game_sequence[0]', '$game_sequence[1]', '$game_sequence[2]')");
 
 	$incentive_sequence = array('badges','levels','leader');
 	shuffle($incentive_sequence);
+	echo $incentive_sequence[0];
+	echo $incentive_sequence[1];
+	echo $incentive_sequence[2];
 	$mysqli->query("INSERT INTO user (id, first_incentive, second_incentive, third_incentive) VALUES ('$SESSION_ID', '$incentive_sequence[0]', '$incentive_sequence[1]', '$incentive_sequence[2]')");
 ?>
