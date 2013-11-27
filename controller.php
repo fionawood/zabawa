a26d2b9760784aff80cc9fbc73fbd1bdbc3479b1
Fiona Wood <fwood@college.harvard.edu>
2013-11-27 12:14:51 -0500

    using isset

--- controller.php
+++ controller.php
@@ -1,5 +1,14 @@
-<?
+<?	
 	session_start();
+	
+
+	if(isset($_SESSION['views']))
+	    $_SESSION['views'] = $_SESSION['views']+ 1;
+	else
+	    $_SESSION['views'] = 1;
+
+	echo "views = ". $_SESSION['views']; 
+
 	include "config.php";
 	$SESSION_ID = $_SESSION['id'];
 	echo "hello";
