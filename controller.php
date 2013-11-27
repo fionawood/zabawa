c15e5db4d1e28725ef4c3807f111e5bf74411357
Fiona Wood <fwood@college.harvard.edu>
2013-11-27 18:40:28 -0500

    clean up

--- controller.php
+++ controller.php
@@ -1,19 +1,14 @@
 <?	
-	echo session_start();
+	session_start();
 
 	if(isset($_SESSION['views']))
 	    $_SESSION['visits'] = $_SESSION['visits']+ 1;
 	else
 	    $_SESSION['visits'] = 1;
-
-	echo "visits = ". $_SESSION['visits']; 
 	$SESSION_VISITS = $_SESSION['visits'];
+	$SESSION_ID = session_id();
 
 	include "config.php";
-	$SESSION_ID = $_SESSION['id'];
-	echo "hello";
-	echo "id = ". $SESSION_ID;
-	echo session_id();
 
 	$mysqli = new mysqli("$dbHost", "$dbUsername", "$dbPass", "$dbName");
 
@@ -22,13 +17,6 @@
 
 	shuffle($game_sequence);
 	shuffle($incentive_sequence);
-
-	echo $game_sequence[0];
-	echo $game_sequence[1];
-	echo $game_sequence[2];
-	echo $incentive_sequence[0];
-	echo $incentive_sequence[1];
-	echo $incentive_sequence[2];
 
 	$mysqli->query("INSERT INTO user (id, visits, game0, game1, game2, incentive0, incentive1, incentive2) VALUES ('$SESSION_ID', '$SESSION_VISITS', '$game_sequence[0]', '$game_sequence[1]', '$game_sequence[2]', '$incentive_sequence[0]', '$incentive_sequence[1]', '$incentive_sequence[2]')");
 ?>
