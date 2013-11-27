9db3326122c0d4755ce56d46a96d5dda32fcd3cf
Fiona Wood <fwood@college.harvard.edu>
2013-11-27 18:30:36 -0500

    add visits to sql

--- controller.php
+++ controller.php
@@ -7,6 +7,7 @@
 	    $_SESSION['visits'] = 1;
 
 	echo "visits = ". $_SESSION['visits']; 
+	$SESSION_VISITS = $_SESSION['visits'];
 
 	include "config.php";
 	$SESSION_ID = $_SESSION['id'];
@@ -16,18 +17,17 @@
 	$mysqli = new mysqli("$dbHost", "$dbUsername", "$dbPass", "$dbName");
 
 	$game_sequence = array('text-adventure','word-bounce','word-scramble');
+	$incentive_sequence = array('badges','levels','leader');
+
 	shuffle($game_sequence);
+	shuffle($incentive_sequence);
+
 	echo $game_sequence[0];
 	echo $game_sequence[1];
 	echo $game_sequence[2];
-
-	$mysqli->query("INSERT INTO user (id, first_game, second_game, third_game) VALUES ('$SESSION_ID', '$game_sequence[0]', '$game_sequence[1]', '$game_sequence[2]')");
-
-	$incentive_sequence = array('badges','levels','leader');
-	shuffle($incentive_sequence);
 	echo $incentive_sequence[0];
 	echo $incentive_sequence[1];
 	echo $incentive_sequence[2];
 
-	$mysqli->query("INSERT INTO user (id, first_incentive, second_incentive, third_incentive) VALUES ('$SESSION_ID', '$incentive_sequence[0]', '$incentive_sequence[1]', '$incentive_sequence[2]')");
+	$mysqli->query("INSERT INTO user (id, visits, game0, game1, game2, incentive0, incentive1, incentive2) VALUES ('$SESSION_ID', '$SESSION_VISITS', '$game_sequence[0]', '$game_sequence[1]', '$game_sequence[2]', '$incentive_sequence[0]', '$incentive_sequence[1]', '$incentive_sequence[2]')");
 ?>
