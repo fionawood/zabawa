f504cb1916f31d25673ce6af12d25a78c58fdd08
Fiona Wood <fwood@college.harvard.edu>
2013-11-27 20:11:43 -0500

    some trials without incentives

--- controller.php
+++ controller.php
@@ -14,29 +14,22 @@
 	$mysqli = new mysqli("$dbHost", "$dbUsername", "$dbPass", "$dbName");
 
 	$game_sequence = array('text-adventure','word-bounce','word-scramble');
+	shuffle($game_sequence);
+
 	$incentive_sequence = array('badges','levels','leader');
+	if(rand()/getrandmax() < .2) $incentive_sequence = array('none','none','none');
+	else shuffle($incentive_sequence);
 
 	$_SESSION['game_sequence'] = $game_sequence;
 	$_SESSION['incentive_sequence'] = $incentive_sequence;
 
-	shuffle($game_sequence);
-	shuffle($incentive_sequence);
+	$_SESSION['pair'] = 0;
+
 
 	$mysqli->query("INSERT INTO user (id, visits, game0, game1, game2, incentive0, incentive1, incentive2) VALUES ('$SESSION_ID', '$SESSION_VISITS', '$game_sequence[0]', '$game_sequence[1]', '$game_sequence[2]', '$incentive_sequence[0]', '$incentive_sequence[1]', '$incentive_sequence[2]')");
 ?>
 
 <script>
-	function makeURL(page) {
-	  var x = "http://zabawa.cs179.org/";
-	  //var x = "file://localhost/Users/fionawwood/Documents/zabawa/";
-	  x=x.concat(page);
-	  x=x.concat(".html");
-	  return x;
-	}
-
-	var firstGame = document.getElementById("firstGame");
-	var game0 = "<?php echo $_SESSION['game_sequence'][0]; ?>";
-	console.log(game0);
-	console.log(firstGame);
-	firstGame.href = makeURL(game0);
+	var game_mode = "<?php echo $_SESSION['game_sequence'][$_SESSION['pair']]; ?>";
+	var incentive_mode = "<?php echo $_SESSION['incentive_sequence'][$_SESSION['pair']]; ?>";
 </script>
