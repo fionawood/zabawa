04957e78ed1a6f587982972e55adf6c047271a3c
Fiona Wood <fwood@college.harvard.edu>
2013-11-29 15:47:31 -0500

    fix view vs visits

--- process_survey.php
+++ process_survey.php
@@ -5,7 +5,7 @@
 	$mysqli = new mysqli("$dbHost", "$dbUsername", "$dbPass", "$dbName");
 
 	$SESSION_ID = $_SESSION['id'];
-	$SESSION_VIEWS = $_SESSION['views'];
+	$SESSION_VISITS = $_SESSION['visits'];
 	$game_mode = $_SESSION['game_sequence'][$_SESSION['pair']];
 	$incentive_mode = $_SESSION['incentive_sequence'][$_SESSION['pair']];
 	$enjoy = $_POST['enjoy'];
@@ -24,9 +24,7 @@
 
 	if(!$mysqli->connect_errno) {
 
-		$mysqli->query("INSERT INTO user (id, visits, game0, game1, game2, incentive0, incentive1 incentive2) VALUES ('$SESSION_ID', '$SESSION_VISITS', '$game_sequence[0]', '$game_sequence[1]', '$game_sequence[2]', '$incentive_sequence[0]', '$incentive_sequence[1]', '$incentive_sequence[2]')");
-
-        $result = $mysqli->query("INSERT INTO game (user, views, game, incentive, time_elapsed, enjoy_q, challenge_q, similar_q, enjoy_similar_q, replay_q, incentive_q)  VALUES ('$SESSION_ID', '$SESSION_VIEWS', '$game_mode', '$incentive_mode', '$time', '$enjoy', '$challenge', '$similar', '$enjoy_similar', $replay', $incentive')");
+        $result = $mysqli->query("INSERT INTO game (user, visits, game, incentive, time_elapsed, enjoy_q, challenge_q, similar_q, enjoy_similar_q, replay_q, incentive_q)  VALUES ('$SESSION_ID', '$SESSION_VISITS', '$game_mode', '$incentive_mode', '$time', '$enjoy', '$challenge', '$similar', '$enjoy_similar', $replay', $incentive')");
         // $mysqli->close();            
         echo $result;     
     }
