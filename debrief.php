70783431fe3b93895cf630e26efb66582b35b1fe
Fiona Wood <fwood@college.harvard.edu>
2013-11-29 23:00:27 -0500

    asf

---
+++ debrief.php
@@ -0,0 +1,67 @@
+<html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>NLP Online Input Character Evaluation</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+
+        <!--[if IE]>
+              <link href="/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+        <![endif]-->
+
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+
+    </head>
+
+    <body>
+    <div class = "container">
+
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+
+        <section class = "exposition">
+
+        <p class = "very-large-text bright centered">Thank you so much! :D</p> 
+
+        <span class = "large-text">
+
+        <p> So we may have been lying a little bit. </p>
+
+        </span>
+
+        <p class = "large-text dark"> <strong>This experiment was not actually about linguistics or anything language-related at all.</strong></p>
+
+        <p>We are investigating <strong>how task engagement changes</strong> depending on the <span class="bright"> <em> intrinsic interest value </em></span> of the given task (for example, finding nouns versus playing a text adventure game) and the <span class="dark"><em>incentive scheme</em></span> applied to it (i.e. badges versus leveling up).</p>
+
+        <p > If you would like to be notified of the results of this study, please enter your e-mail:
+
+        <input type="email" pattern="[^ @]*@[^ @]*" value="">
+        <input type="submit" value="Submit"/>
+
+        </p>
+
+        </section>
+
+
+
+
+    </div> <!-- end container div -->  
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+
+
+    </body>
+</html>
