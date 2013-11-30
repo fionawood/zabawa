76b46a1fb232110c1f752d7f7bd55a68a9876edf
Fiona Wood <fwood@college.harvard.edu>
2013-11-29 23:18:14 -0500

    advance properly

--- process_time.php
+++ process_time.php
@@ -9,9 +9,11 @@
     $t =  $_GET['elapsed'];
     $SESSION_ID = $_SESSION['id'];
     $SESSION_VISITS = $_SESSION['visits'];
-	$game_mode = $_SESSION['game_sequence'][$_SESSION['pair']];
-	$incentive_mode = $_SESSION['incentive_sequence'][$_SESSION['pair']];
+	// $game_mode = $_SESSION['game_sequence'][$_SESSION['pair']];
+	// $incentive_mode = $_SESSION['incentive_sequence'][$_SESSION['pair']];
 
-    $result = $mysqli->query("INSERT INTO game (id, visits, game, incentive, time_elapsed)  VALUES ('$SESSION_ID', '$SESSION_VISITS', $game_mode, $incentive_mode, '$t')");
+    $result = $mysqli->query("INSERT INTO game (id, visits, time_elapsed)  VALUES ('$SESSION_ID', '$SESSION_VISITS', '$t')");
+
+    // $result = $mysqli->query("INSERT INTO game (id, visits, game, incentive, time_elapsed)  VALUES ('$SESSION_ID', '$SESSION_VISITS', $game_mode, $incentive_mode, '$t')");
 
 ?>
