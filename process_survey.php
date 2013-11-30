db6ad58a074fe2fb3e0fc9277861e2f7fd0f16fd
Fiona Wood <fwood@college.harvard.edu>
2013-11-29 23:43:45 -0500

    urgg

--- process_survey.php
+++ process_survey.php
@@ -8,20 +8,18 @@
 	$SESSION_VISITS = $_SESSION['visits'];
 	$game_mode = $_SESSION['game_sequence'][$_SESSION['pair']];
 	$incentive_mode = $_SESSION['incentive_sequence'][$_SESSION['pair']];
-	$enjoy = $_POST['enjoy'];
-	$challenge = $_POST['challenge'];
-	$similar = $_POST['similar'];
-	$enjoy_similar = $_POST['enjoy-similar'];
-	$replay = $_POST['replay'];
+	// $enjoy = $_POST['enjoy'];
+	// $challenge = $_POST['challenge'];
+	// $similar = $_POST['similar'];
+	// $enjoy_similar = $_POST['enjoy-similar'];
+	// $replay = $_POST['replay'];
+	// if(!empty($_POST["incentive"]))
+	// 	$incentive = $_POST['incentive'];
+	// else
+	// 	$incentive = 0;
 
 	$elapsed = $_SESSION['time_elapsed'];
 
-	if(!empty($_POST["incentive"]))
-		$incentive = $_POST['incentive'];
-	else
-		$incentive = 0;
-
-	$time = 0;
 
 	if(!$mysqli->connect_errno) {
 		$mysqli->query("INSERT INTO game (id, visits, time_elapsed)  VALUES ('$SESSION_ID', '$SESSION_VISITS', '$elapsed')");
