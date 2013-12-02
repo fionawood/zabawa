a848386abc7830de1c50c3927887455223a6eece
Fiona Wood <fwood@college.harvard.edu>
2013-12-02 17:28:30 -0500

    pren

--- js/incentives.js
+++ js/incentives.js
@@ -126,7 +126,7 @@ var assign_badge_word = function(word) {
             description = 'Five letters!';
             $(".badge-modal").html("<h5> You've earned a badge! </h5> <h2>" + w + "</h2> <br/>" + img + "<br/> <p>" + description + "</p>").hide().fadeIn(300).delay(800).fadeOut(1000);
             $(".incentives #badges ul").append("<li>" + img + "<h3 class = 'badge-name'>" + w + "</h3> </li>");
-        } else if((word.length()==7) {
+        } else if(word.length()==7) {
             img = '<img src=\'img/seven.svg\' width=40 height=40>';
             description = 'Seven letters!';
             $(".badge-modal").html("<h5> You've earned a badge! </h5> <h2>" + w + "</h2> <br/>" + img + "<br/> <p>" + description + "</p>").hide().fadeIn(300).delay(800).fadeOut(1000);
