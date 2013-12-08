c74d8738a7f3c38d62001e9d1813e9d3ff580160
Fiona Wood <fwood@college.harvard.edu>
2013-12-08 01:11:02 -0500

    testing

--- js/word-scramble.js
+++ js/word-scramble.js
@@ -91,7 +91,9 @@ var process_input = function(cmd_text) {
         scrambled.splice(current,1);
 
     } else {
+        console.log(scrambled[current]);
         scrambled[current] = scrambled[current].split('').sort(function(){return 0.5-Math.random()}).join('');
+        console.log(scrambled[current]);
         if(current<ws.length-1) current++;
         else current=0;
     }
